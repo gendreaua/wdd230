b58948415f2351f16cbc33d9b7f7eaae7107b187
gendreaua <fel16012@byui.edu>
2021-01-09 22:37:48 -0800

    homepage beg

---
+++ js/modified.js
@@ -0,0 +1,4 @@
+let m = document.lastModified;
+document.getElementById("output").textContent = "Last Updated: " + m;
+let date = new Date();
+document.getElementById("currentYear").textContent = date.getFullYear();
